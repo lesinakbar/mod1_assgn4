b3c65214032a076c6690a179661d445e38d8f436
lesinakbar <lesinakbar@gmail.com>
2016-07-25 20:56:13 +0530

    Create Assgn4.java

---
+++ Assgn4.java
@@ -0,0 +1,21 @@
+/* Date : 22/07/2016
+ * Assume that the integer variable x is 5 and the integer variable y is 10. Give the values of the following
+expressions:
+a. x + y * 2
+b. x - y + 2
+c. (x+y) * 2
+d. y % x
+ */
+public class Assgn4 {
+
+	public static void main(String[] args) {
+		
+	int x=5,y=10;
+	System.out.println("When x = 5 and y = 10\n");
+	System.out.println("a. x + y * 2 = " + (x + y * 2));
+	System.out.println("b. x - y + 2 = " + (x - y + 2));
+	System.out.println("c. (x+y) * 2 = " + ((x+y) * 2));
+	System.out.println("d. y % x = " + (y % x));
+	}
+
+}
